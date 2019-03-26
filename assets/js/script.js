d5b4139a709b51e53e68b7ef066b14df484b6a35
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-03-25 19:58:40 -0500

    this is the basic time pattern I am going to be using I have variables undefined for the time being but I have an idea as to what I am going to use index for

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+	var index = 0;
+	var countdownTimer = {
+		time : 30,
+		reset: function() {
+			this.time = 30;
+			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
+		},
+		start: function() {
+			counter = setInterval(countdownTimer.count, 1000);	
+		},
+		stop: function() {
+			clearInterval(counter);
+		},
+		count: function() {
+				countdownTimer.time--;
+				console.log(countdownTimer.time);
+//				$('.timer').html(countdownTimer.time);
+		} }});
